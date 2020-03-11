ba5e3e498a4c63b3e614468016b090732a392c7d
Harrison Ngan <hngan@Harrisons-MacBook-Pro.local>
2020-03-10 20:36:06 -0400

    Added logout

--- client/src/components/home/LoggedIn.jsx
+++ client/src/components/home/LoggedIn.jsx
@@ -4,7 +4,8 @@ import Axios from 'axios';
 export default class HomeScreen extends React.Component{
     logout = (event)=>{
         event.preventDefault();
-        cookies.remove('token');
+        document.cookie = 'token=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        this.props.history.push('/login');
     }
     render(){
         return(
