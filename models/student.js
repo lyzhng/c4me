00342d38408da55fe77942dda1b4dc4f38fc13eb
Harrison Ngan <hngan@Harrisons-MacBook-Pro.local>
2020-03-10 22:55:08 -0400

    Updated student schema to reflect the student csv files

--- models/student.js
+++ models/student.js
@@ -1,24 +1,35 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
+Student.sat.math = 700; //my way
+Student.SAT_MATH_1 = 700; // stoller
 
 const StudentSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
+    userid: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    sat:{
-        math:Number,
-        ebrw:Number,
-        literature:Number,
-        MathI:Number,
-        MathII:Number,
-        usHist:Number,
-        worldHist:Number,
-        
-        },
-    // act:{type: Schema.Types.ObjectId, ref:'Act'},
+    SAT_math: Number,
+    SAT_ebrw: Number,
+    SAT_literature: Number,
+    SAT_math_I: Number,
+    SAT_math_II: Number,
+    SAT_US_hist: Number,
+    SAT_world_hist: Number,
+    SAT_eco_bio: Number,
+    SAT_mol_bio: Number,
+    ACT_English: Number,
+    ACT_math: Number,
+    ACT_reading: Number,
+    ACT_composite: Number,
+    location: String,
+    major_1: String,
+    major_2: String,
+    college_class: Number,
+    num_AP_passed: Number,
+    high_school_city: String,
+    high_school_sate: String,
+    high_school_name: String,
     // applications:[{type: Schema.Types.ObjectId, ref:'Application'}],
-    location:{type:String, default:""},
   });
 
 StudentSchema.pre('save', function(next) {
