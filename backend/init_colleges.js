0d85bf64ad4536c122e0c9629620d0364e2ad66e
samuel <samuechen@cs.stonybrook.edu>
2020-03-11 00:24:31 -0400

    added function to populate college database from colleges.txt

---
+++ backend/init_colleges.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const college = require('../models').College;
+
+const request = require("request");
+
+const getCollegeNames = require("./get_college_names.js");
+
+mongoose.connect("mongodb://localhost/c4me", { useUnifiedTopology: true, useNewUrlParser: true });
+
+
+module.exports = function()
+{
+	console.log("hello");
+	return new Promise (function(resolve, reject)
+	{
+		college.find(async function(err, collegeArr)
+		{
+			if (err)
+			{
+				console.log("error with the database (init_colleges.js)");
+				reject();
+			}
+			else
+			{
+				if (collegeArr.length != 0)
+				{
+					console.log("colleges have already been initialized");
+					reject();
+				}
+				else
+				{
+					let collegeNames = await getCollegeNames();
+					for (let i = 0; i < collegeNames.length; i ++)
+					{
+						await new Promise(function(resolve, reject)
+						{
+							college.create({name : collegeNames[i]}, function (err)
+							{
+								if (err)
+								{
+									console.log("error with the database (init_colleges.js)");
+								}
+								else
+								{
+									console.log("created college: " + collegeNames[i]);
+								}
+								resolve();
+							});
+						});
+					}
+					resolve();
+				}
+			}
+		});
+	});
+}
+
+module.exports();
