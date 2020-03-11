1799b781a889868c96164f02d0f440053ad7a745
samuel <samuechen@cs.stonybrook.edu>
2020-03-10 23:00:35 -0400

    fixed regex

--- backend/get_college_names.js
+++ backend/get_college_names.js
@@ -13,8 +13,32 @@ module.exports = function()
 			}
 			else
 			{
-				resolve(contents.split(/(\\r\\n|\\r|\\n)/).filter((str, i) => {return i % 2 == 0}));
+				resolve(contents.split(/(\r\n|\r|\n)/).filter((str, i) => {return i % 2 == 0}));
 			}
 		});
 	});
 }
+
+//returns array of college names
+// [ 'American University',
+//   'Barnard College',
+//   'Berry College',
+//   'California State University, East Bay',
+//   'California State University, Fresno',
+//   'California State University, Monterey Bay',
+//   'Campbell University',
+//   'Carnegie Mellon University',
+//   'Central Connecticut State University',
+//   'Centre College',
+//   'Clarkson University',
+//   'Colgate University',
+//   'Colorado College',
+//   'DePaul University',
+//   'DePauw University',
+//   'Drake University',
+//   'Drexel University',
+//   'Eastern Illinois University',
+//   'Eastern Washington University',
+//   'Florida Gulf Coast University',
+//   'Fordham University'...
+//   ]
