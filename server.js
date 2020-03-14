87e4caf2cb0de204d221129a3d75147d5736d118
Harrison Ngan <hngan@Harrisons-MacBook-Pro.local>
2020-03-14 01:19:21 -0400

    Added backend routes for admin functions

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const cookieParser = require('cookie-parser');
 const jwtAuth = require('./jwt_middleware'); 
 const axios = require('axios');
 const collections = require('./models');
+const backend = require('./backend');
 
 const PORT = process.env.PORT || 3001;
 const secret = process.env.JWT_SECRET_KEY || "pokTGERW54389e#@$%mans12$@!$!#$^#%$";
+
+const studentDatasets = ["students-1.csv"];
+const applicationDatasets = ["applications-1.csv"];
 
 app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -21,7 +25,6 @@ app.get("/", (req, res)=>{
 });
 
 app.post("/api/register", (req, res)=>{
-  console.log("hi");
   collections.Student.find({userid:req.body.userid}).lean().then((resp) => {
     if(resp.length === 0){
       collections.Student.create(req.body).then((collectionsmodel)=>{
@@ -37,7 +40,6 @@ app.post("/api/register", (req, res)=>{
 
 app.post("/api/login", (req, res)=>{
   const { userid, password } = req.body;
-  console.log(req.body);
     collections.Student.findOne({ userid }).then((user)=>{
       if (!user) {
         res.status(401).json({error: 'Incorrect userid or password'});
@@ -60,9 +62,33 @@ app.post("/api/login", (req, res)=>{
     });
 });
 
+app.get("/deletestudents", (req, res)=>{
+  backend.adminHandler.deleteAllStudents();
+  res.status(200).send();
+});
+
+app.get("/importstudentdatasets", (req, res)=>{
+  backend.adminHandler.importStudentProfiles(studentDatasets[0]);
+});
+
+app.get("/scrapecollegerankings", (req, res)=>{
+  backend.adminHandler.importCollegeRankings();
+  res.status(200).send();
+});
+
+app.get("/scrapecollegedata", (req, res)=>{
+  backend.adminHandler.importCollegeGPA();
+  res.status(200).send();
+});
+
+app.get("/importcollegescorecard", (req, res)=>{
+  backend.adminHandler.importScorecardData();
+  res.status(200).send();
+});
+
 //CHECKS IF WE ARE LOGGED IN OR NOT WITH MIDDLEWARE
 app.get('/checkToken', jwtAuth, (req, res)=>{
-  res.sendStatus(200);
+  res.sendStatus(200).send();
 });
 
 
