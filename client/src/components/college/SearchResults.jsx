0065e48b39ba3c1b6c1357700dfed4115eabeb4f
Harrison Ngan <hngan@Harrisons-MacBook-Pro.local>
2020-03-15 18:01:00 -0400

    Connected admin page to backend

--- client/src/components/college/SearchResults.jsx
+++ client/src/components/college/SearchResults.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 
 export default class SearchResults extends React.Component {
     state = {
-        name:"",
+    	name:"",
     }
 
     componentDidMount(){
-
+    	this.setState({name: this.props.name});
     }
 
     render(){
         <div>
-            <h3>{this.state.name}</h3>
-
+          <h3>{this.state.name}</h3>
         </div>
     }
 }
