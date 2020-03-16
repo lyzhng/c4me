019093fe8f86e5dad3d4b8225dc2e39bea59a749
Harrison Ngan <hngan@Harrisons-MacBook-Pro.local>
2020-03-15 23:43:09 -0400

    Changed headers for scraping niche

--- backend/admin_handler.js
+++ backend/admin_handler.js
@@ -528,6 +528,7 @@ remapped_names.set('Franklin and Marshall College', 'Franklin Marshall College')
 remapped_names.set('SUNY College of Environmental Science and Forestry', 'State University of New York College of Environmental Science and Forestry');
 remapped_names.set('The College of Saint Scholastica', 'College of St. Scholastica');
 
+
 const scrapeAcademicRanking = ($) => {
 	let grades = $("#report-card .card--profile .report-card .profile__buckets .profile__bucket--2 .ordered__list__bucket li .profile-grade--two").map(function(){return $(this).text()}).get();
 	//grades[0] returns AcademicsA-, e.g. AcademicsA+, so take substring starting with index 9
@@ -581,21 +582,17 @@ const scrapeSimilarAppliedColleges = ($) => {
  return popularCollegeList;
 };
 
-let userAgentsIndex = 0;
-
 const importHighschoolData = (name, city, state) => {
 	let url ="https://www.niche.com/k12/"+ name + "-" +city + "-" +state;
 	url = url.split(" ").join("-");
 	axios.get(url, 
 	{headers:{
 		'Host': 'www.niche.com',
-		'User-Agent':userAgents[userAgentsIndex++],
+		'User-Agent':userAgents[Math.floor(Math.random() * 49)],
 		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 		'Accept-Language': 'en-US,en;q=0.5',
 		'Accept-Encoding': 'gzip, deflate, br',
 		'DNT': '1',
-		'Connection': 'keep-alive',
-		'Cookie': '_pxhd=b03406b8705b7f8353ccba6e98571777c6f328d316bfaf6f533f0682adde7bb3:4cc57971-6648-11ea-8e32-4bc99936e254; _pxhd=b03406b8705b7f8353ccba6e98571777c6f328d316bfaf6f533f0682adde7bb3:4cc57971-6648-11ea-8e32-4bc99936e254; xid=dabb0501-8a9d-4bd3-ac3b-f95e850e490e; navigation=%7B%22location%22%3A%7B%22guid%22%3A%2214b82916-cfce-4bae-a87e-d5610ef2f8a6%22%2C%22type%22%3A%22County%22%2C%22name%22%3A%22St.%20Francois%20County%22%2C%22url%22%3A%22st-francois-county-mo%22%7D%2C%22navigationMode%22%3A%22full%22%2C%22vertical%22%3A%22k12%22%2C%22mostRecentVertical%22%3A%22k12%22%2C%22suffixes%22%3A%7B%22colleges%22%3A%22%2Fs%2Fmissouri%2F%22%2C%22graduate-schools%22%3A%22%2Fs%2Fmissouri%2F%22%2C%22k12%22%3A%22%2Fc%2Fst-francois-county-mo%2F%22%2C%22places-to-live%22%3A%22%2Fc%2Fst-francois-county-mo%2F%22%2C%22places-to-work%22%3A%22%2Fc%2Fst-francois-county-mo%2F%22%7D%7D; experiments=mini_expansion_header%7Cvariation1%5E%5E%5E%240%7C1%5D; _gcl_au=1.1.1273538567.1584227130; niche_sessionPageCount=16; niche_npsSurvey=0; niche_fullStory=0; niche_singleFirstPageview=1; _ga=GA1.2.1996521713.1584227130; _gid=GA1.2.484543382.1584227130; _fbp=fb.1.1584227130694.666677108; _cmpQcif3pcsupported=1; _scid=38a247e1-2e5a-4858-9a44-2910b5661ce8; _pxvid=4cc57971-6648-11ea-8e32-4bc99936e254; _sctr=1|1584158400000; niche_singleProfilePageview=1; niche_singleK12Pageview=1; recentlyViewed=entityHistory%7CentityName%7CCentral%2BHigh%2BSchool%7CentityGuid%7C88d1fd1f-bafd-4257-9d52-ec3ec7e0c298%7CentityType%7CSchool%7CentityFragment%7Ccentral-high-school-park-hills-mo%7CStratford%2BHigh%2BSchool%7C4a2b0a34-a0e3-4add-90ce-92c5f610d200%7Cstratford-high-school-stratford-tx%7CCobalt%2BInstitute%2Bof%2BMath%2B%26%2BScience%7C72cf8eaa-7e3c-4f05-82bc-cf11d26b56c5%7Ccobalt-institute-of-math--and--science-victorville-ca%7CThe%2BQueens%2BSchool%2Bof%2BInquiry%7Cf116a725-9b62-437c-ac29-3b5801e29ee1%7Cthe-queens-school-of-inquiry-flushing-ny%7CsearchHistory%7CSt.%2BFrancois%2BCounty%7C14b82916-cfce-4bae-a87e-d5610ef2f8a6%7CCounty%7Cst-francois-county-mo%7CSherman%2BCounty%7C76b68cf8-bdb9-4b94-b60e-fa58e70359f1%7Csherman-county-tx%7CSan%2BBernardino%2BCounty%7C97af0462-bff4-4231-85f5-b7f95c1a7fa9%7Csan-bernardino-county-ca%5E%5E%5E%240%7C%40%241%7C2%7C3%7C4%7C5%7C6%7C7%7C8%5D%7C%241%7C9%7C3%7CA%7C5%7C6%7C7%7CB%5D%7C%241%7CC%7C3%7CD%7C5%7C6%7C7%7CE%5D%7C%241%7CF%7C3%7CG%7C5%7C6%7C7%7CH%5D%5D%7CI%7C%40%241%7CJ%7C3%7CK%7C5%7CL%7C7%7CM%5D%7C%241%7CN%7C3%7CO%7C5%7CL%7C7%7CP%5D%7C%241%7CQ%7C3%7CR%7C5%7CL%7C7%7CS%5D%5D%5D; profileModalPromptShown=true; __gads=ID=e5c3d5ccf60cad4a:T=1584227585:S=ALNI_MbKnoXV93C7y94k0nsVIk3Kb5UbKA; G_ENABLED_IDPS=google; hintSeenLately=true; _dc_gtm_UA-2431522-39=1; +fCF0WpZQ4w2cRNE5j40p6w60qQ6sfap3mIzI4BabJc=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJuaWNoZS5jb20iLCJleHAiOjE1OTIwMDM5MjcsImlhdCI6MTU4NDIyNzkyNywic3ViIjoiNjU5MDE5MzMtNjgxMC00ZTFkLWIxOWYtOThmZTRlYzRiNzBiIiwic2NvcGUiOiJsb2dpbiJ9.rwvr-8L341xLgDwCoty0on5JkErkMpan-xF-_rwAUZjU86-MVFbOJUGpALwVuQTKVeyc1gnpQ5RZ0LBbxZ98KHbk9PkWN-WuVqLJVWaH0-fsR5_J801ICpFYBO3WHmsdw1ft_35wJ7DwV7h--LMs-YMarb_J-pSy8vjFXeXeQXm-beECzskAe-Y_6VfQfNBZcatlpo0NWq0qYrIKtPCkgtMYZEgs_x0JkZESbkG1F6MXWovdSSDynC17AUsV0GahlcMSSmG6D6B-iq03TQEiobJuC_wKoBuLrYnV2yYZLVdjSEp-U3ckl8y2e2nWGuyeFwB3T-dZxjcUODIeFU79dw; emailVerificationPrompts=2; _pxff_wa=1; _pxff_tm=1',
 		'Upgrade-Insecure-Requests': '1',
 		'TE': 'Trailers'
 	}}
@@ -615,7 +612,7 @@ const importHighschoolData = (name, city, state) => {
 	highschool.college_prep_ranking = scrapeCollegePrepRanking(html);
 	highschool.similar_colleges_applied = scrapeSimilarAppliedColleges(html);
 	collections.HighSchool.create(highschool).then(resp => {
-		console.log("Created:", resp);
+		//console.log("Created:", resp);
 	}).catch(err =>{ console.log(err) });
 	}).catch((err)=>{
 		console.log("Scrape for high school:",name, city, state, "Gave the following error:",err.response.status, err.response.statusText);
@@ -638,7 +635,7 @@ module.exports = {
 //importCollegeRankings();
 //deleteAllStudents();
 //importCollegeDescriptions();
-importCollegeData();
+//importCollegeData();
 //importHighschoolData("blah", "blah", "blah");
 //importHighschoolData("central high school", "park hills", "mo");
 // importHighschoolData("Ward Melville Senior High School", "East Setauket", "ny");
