b7ec021fbb99ab05555dfc7fb8b8770abde6171d
Lily Zhong <lilyzhongy@gmail.com>
2020-03-22 13:29:25 -0400

    refactor import college descriptions test case

--- test/import_THE.test.js
+++ test/import_THE.test.js
@@ -26,26 +26,12 @@ describe('import college rankings', () => {
       assert.typeOf(ranking, 'number');
     });
   });
-});
-
-describe('import college descriptions', () => {
-  before(async () => {
-    mongoose.connect("mongodb://localhost/c4me", { useUnifiedTopology: true, useNewUrlParser: true });
-    try {
-      await collections.College.collection.drop();
-    } catch (err) {
-      console.log('cannot drop college database');
-    }
-  })
   it('should import college descriptions from THE', async function () {
     this.timeout(0);
-    await importCollegeRankings('./datasets/colleges.txt');
-    const collegeCount = await collections.College.countDocuments({});
-    assert.equal(collegeCount, 101);
     const colleges = await collections.College.find({});
     colleges.forEach((college) => {
       const { description } = college;
       assert.typeOf(description, 'string');
     });
   });
-})
+});
