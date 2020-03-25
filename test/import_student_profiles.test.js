f684f5bc8d775a2ca381d475c0f1d4927bbb20b2
hngan <hngan@cs.stonybrook.edu>
2020-03-25 17:21:25 -0400

    Resolving merge conflict

--- test/import_student_profiles.test.js
+++ test/import_student_profiles.test.js
@@ -3,9 +3,33 @@ const collections  = require('../models');
 const describe = require('mocha').describe;
 const it = require('mocha').it;
 const assert = require('chai').assert;
+const { importStudentProfiles } = require('../backend/admin_handler');
 
-mongoose.connect("mongodb://localhost/c4me", { useUnifiedTopology: true, useNewUrlParser: true });
 
-describe('Import Student Profiles', function() {
-  
+describe('import Student Profiles', function() {
+	before(async () => {
+    mongoose.connect("mongodb://localhost/c4me", { useUnifiedTopology: true, useNewUrlParser: true });
+    try {
+      await collections.Student.collection.drop();
+    } catch (err) {
+      console.log('cannot drop student database');
+    }
+	});
+	
+	it('should have imported 5 students', async function(){
+		await importStudentProfiles("students-1.csv","applications-1.csv")
+		let studentCount = await collections.Student.count({});
+		assert.equal(studentCount, 5)
+	});
+
+	it('should have imported 7 applications', async function(){
+		let studentCount = await collections.Student.count({});
+		assert.equal(studentCount, 5)
+	});
+
+	it('should have added 4 high schools', async function(){
+		let studentCount = await collections.Student.count({});
+		assert.equal(studentCount, 5)
+	});
+
 });
