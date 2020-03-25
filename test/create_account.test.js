5fffbeaa07e446077ca614ba36b56108ad3ab6d8
hngan <hngan@cs.stonybrook.edu>
2020-03-25 19:30:00 -0400

    Added account creation testing

---
+++ test/create_account.test.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const collections = require('../models');
+// const describe = require('mocha').describe;
+// const it = require('mocha').it;
+const assert = require('chai').assert;
+const { registerStudent } = require('../backend/student_handler.js');
+
+describe("create new account",function(){
+	before(async () => {
+		mongoose.connect("mongodb://localhost/c4me", { useUnifiedTopology: true, useNewUrlParser: true });
+		try {
+			await collections.Student.collection.drop();
+		} catch (err) {
+		  console.log('cannot drop student database');
+		}
+		});
+	
+	it('should create a student with userid newStudent1', async () => {
+		const response = await registerStudent("newstudent1", "password");
+		let student = await collections.Student.find({userid:"newstudent1"});
+		assert.equal(student.length, 1);
+	});
+
+	it('should deny registration if userid is already taken', async () => {
+		try{
+			let response = await registerStudent("newstudent1", "password");
+			assert.equal(response, "userid or email already taken!");
+		}catch(err){
+			assert.equal(err.message, "userid or email already taken!")
+		}
+	});
+
+	after(() => {
+		console.log("Finished testing account creation.")
+	});
+});
