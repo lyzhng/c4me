72916ed2b3fe2be6cf78b7188b290a13114d5cf3
samuel <samuechen@cs.stonybrook.edu>
2020-04-13 03:51:21 -0400

    added more info

--- client/src/components/college/SearchResults.jsx
+++ client/src/components/college/SearchResults.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import AppTracker from '../apptracker/AppTracker';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
 
 function AppTrackerModal(props)
 {
@@ -15,7 +17,7 @@ function AppTrackerModal(props)
         Launch AppTracker
       </Button>
 
-      <Modal show={show} onHide={handleClose} animation={false}>
+      <Modal show={show} onHide={handleClose} animation={false} size = "lg">
         <Modal.Body>
           <AppTracker college = {props.college}/>
         </Modal.Body>
@@ -43,17 +45,34 @@ export default class SearchResults extends React.Component {
     render(){
       if(this.props.college){
         return(
-          <div style = {{border : this.props.college.hidden ?  "2px dashed red" : ""}}>
+          <div style = {{display : this.props.college.hidden ?  "none" : ""}}>
             <h3>{this.props.college.name} rank: {this.props.college.ranking}</h3>
             <div>{this.props.college.location.city}, {this.props.college.location.state}</div>
-            <div> size: {this.props.college.size}</div>
-            <div> SAT Math avg: {this.props.college.sat.math_avg}</div>
-            <div> SAT Eng avg: {this.props.college.sat.EBRW_avg}</div>
-            <div> ACT avg: {this.props.college.act.avg}</div>
-            <div> cost of attendance: {this.props.college.cost.attendance.in_state}</div>
-            <div> admission_rate: {this.props.college.admission_rate}</div>
             <AppTrackerModal college = {this.props.college}/>
-            {/* <div> majors: {this.props.college.majors.reduce((total, current)=>{return total + "|" + current;})}</div> */}
+
+            <Accordion>
+              <Card>
+                <Card.Header>
+                  <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                    More Info
+                  </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>
+                    <div> size: {this.props.college.size}</div>
+                    <div> SAT Math avg: {this.props.college.sat.math_avg}</div>
+                    <div> SAT Eng avg: {this.props.college.sat.EBRW_avg}</div>
+                    <div> ACT avg: {this.props.college.act.avg}</div>
+                    <div> tuition (instate/outstate): {this.props.college.cost.tuition.in_state} / {this.props.college.cost.tuition.out_state}</div>
+                    <div> cost of attendance (instate/outstate): {this.props.college.cost.attendance.in_state} / {this.props.college.cost.attendance.out_state}</div>
+                    <div> admission_rate: {this.props.college.admission_rate}</div>
+                    <div> graduation debt median: {this.props.college.grad_debt_mdn}</div>
+                    <div> graduation rate: {this.props.college.completion_rate}</div>
+                    <div> majors: {this.props.college.majors.reduce((total, current)=>{return total + "|" + current;})}</div>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
           </div>
         )
       }
