cea699d10fd7f3f8baaffe09a5198d193b489793
Lily Zhong <lilyzhongy@gmail.com>
2020-04-23 16:43:58 -0400

    update apptracker ui

--- client/src/components/apptracker/AppTracker.jsx
+++ client/src/components/apptracker/AppTracker.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Axios from 'axios';
+import { Form, Col, Button } from 'react-bootstrap';
 import AppTrackerItem from './AppTrackerItem';
 import Scatterplot from './Scatterplot';
 
@@ -19,8 +20,8 @@ export default class AppTracker extends React.Component {
 
     setDefaultState = () => {
         this.setState({
-            minCollegeClass: this.state.minCollegeClass === '' ? Number.MIN_SAFE_INTEGER : this.state.minCollegeClass,
-            maxCollegeClass: this.state.maxCollegeClass === '' ? Number.MAX_SAFE_INTEGER : this.state.maxCollegeClass,
+            minCollegeClass: this.state.minCollegeClass === '' ? 0 : this.state.minCollegeClass,
+            maxCollegeClass: this.state.maxCollegeClass === '' ? 3000 : this.state.maxCollegeClass,
         });
     }
 
@@ -37,9 +38,6 @@ export default class AppTracker extends React.Component {
         const students = this.state.students;
         for (const s of students) {
             const fitsCriteria = this.appFitsReq(s) && this.highSchoolFitsReq(s) && this.collegeClassFitsReq(s);
-            // console.log('App Status Fits?', this.appFitsReq(s));
-            // console.log('High School Fits?', this.highSchoolFitsReq(s));
-            // console.log('College Class Fits?', this.collegeClassFitsReq(s));
             s.hidden = fitsCriteria ? false : true;
         }
         this.setState({ students: students, scatterplot: !this.state.scatterplot }, () => {
@@ -50,9 +48,6 @@ export default class AppTracker extends React.Component {
     collegeClassFitsReq = (student) => {
         const collegeClass = student.college_class;
         const { minCollegeClass, maxCollegeClass } = this.state;
-        // console.log('College Class of Student', collegeClass);
-        // console.log('Min College Class', minCollegeClass);
-        // console.log('Max College Class', maxCollegeClass);
         return minCollegeClass <= collegeClass && collegeClass <= maxCollegeClass;
     }
     
@@ -83,15 +78,25 @@ export default class AppTracker extends React.Component {
         return false;
     }
 
+    stylizeHoveredText = (e) => {
+        const targetName = e.target.getAttribute('name');
+        document.querySelector(`span[name="${targetName}"]`).style.fontWeight = '800';
+    }
+
+    resetStyle = (e) => {
+        const targetName = e.target.getAttribute('name');
+        document.querySelector(`span[name="${targetName}"]`).style.fontWeight = '400';
+    }
+
     addHighSchool = () => {
         this.setState({ highSchools: new Set([...this.state.highSchools, this.state.currentHighSchool]) });
+        this.setState({ currentHighSchool: '' })
     }
 
     removeHighSchool = (e) => {
-        e.preventDefault();
         const { highSchools } = this.state;
-        const name = document.querySelector('.high-school-unit > .high-school-name').textContent;
-        highSchools.delete(name);
+        const targetName = e.target.getAttribute('name');
+        highSchools.delete(targetName);
         this.setState({ highSchools });
     }
 
@@ -123,59 +128,129 @@ export default class AppTracker extends React.Component {
             <div>
                 <div className="filters">
                     <h2>App Tracker for {this.props.college.name}</h2>
-                    {/* college class */}
-                    <div className="row">
-                        <label htmlFor="minCollegeClass">Min. College Class</label>
-                        <input type="number" name="minCollegeClass" min="0" onChange={this.handleChange} />
-                    </div>
-                    <div className="row">
-                        <label htmlFor="maxCollegeClass">Max. College Class</label>
-                        <input type="number" name="maxCollegeClass" id="" min="0" onChange={this.handleChange} />
-                    </div>
-                    {/* high schools */}
-                    <div className="row">
-                        <label htmlFor="currentHighSchool">High Schools</label>
-                        <input type="text" name="currentHighSchool" id="" value={this.state.currentHighSchool} onChange={this.handleChange} className="current-hs"/>
-                        <button type="submit" onClick={this.addHighSchool}
-                            disabled={!this.state.currentHighSchool.match(/^([\w\-\,\.]+\s*)+$/g)} className="btn-add-hs">Add High School</button>
-                    </div>
-                    <div className="high-school-list">
-                        {
-                            [...this.state.highSchools].map((highSchool) => {
-                                return (
-                                    <div key={highSchool} className="high-school-unit">
-                                        <h5 className="high-school-name">{highSchool}</h5>
-                                        <button onClick={this.removeHighSchool}>Remove High School</button>
-                                    </div>
-                                );
-                            })
-                        }
-                    </div>
-                    <div className="row">
-                        <label htmlFor="appStatuses">Application Statuses:</label>
-                        <input type="checkbox" name="pending" id="" onChange={this.handleAppStatuses} />
-                        <label htmlFor="pending">Pending</label>
-                        <input type="checkbox" name="accepted" id="" onChange={this.handleAppStatuses} />
-                        <label htmlFor="accepted">Accepted</label>
-                        <input type="checkbox" name="denied" id="" onChange={this.handleAppStatuses} />
-                        <label htmlFor="denied">Denied</label>
-                        <input type="checkbox" name="deferred" id="" onChange={this.handleAppStatuses} />
-                        <label htmlFor="deferred">Deferred</label>
-                        <input type="checkbox" name="wait-listed" id="" onChange={this.handleAppStatuses} />
-                        <label htmlFor="wait-listed">Wait-listed</label>
-                        <input type="checkbox" name="withdrawn" id="" onChange={this.handleAppStatuses} />
-                        <label htmlFor="withdrawn">Withdrawn</label>
-                    </div>
-                    <button onClick={this.filter} className="btn-filter">Apply Filters</button>
-                    <label htmlFor="scatterplot">Scatterplot: </label>
-                    <input type="checkbox" name="scatterplot" id="" onChange={this.toggleScatterplot} checked={this.state.scatterplot} />
+                    <Form>
+                        <Form.Group>
+                            <Form.Row>
+                                <Form.Label column lg="3">Minimum College Class</Form.Label>
+                                <Col>
+                                    <Form.Control
+                                        className="form-control"
+                                        title="Enter the minimum college class for the lower bound filter."
+                                        name="minCollegeClass"
+                                        type="tel"
+                                        min="0"
+                                        pattern="[1-2][0-9][0-9][0-9]"
+                                        autoComplete="off"
+                                        onChange={this.handleChange} />
+                                </Col>
+                            </Form.Row>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Row>
+                                <Form.Label column lg="3">Maximum College Class</Form.Label>
+                                <Col>
+                                    <Form.Control
+                                        className="form-control"
+                                        title="Enter the Maximum college class for the lower bound filter."
+                                        name="minCollegeClass"
+                                        type="tel"
+                                        min="0"
+                                        pattern="[1-2][0-9][0-9][0-9]"
+                                        autoComplete="off"
+                                        onChange={this.handleChange} />
+                                </Col>
+                            </Form.Row>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Row>
+                                <Form.Label column lg="3">High Schools</Form.Label>
+                                <Col>
+                                    <Form.Control
+                                        type="text"
+                                        name="currentHighSchool"
+                                        value={this.state.currentHighSchool}
+                                        onChange={this.handleChange}
+                                        className="current-hs" />
+                                </Col>
+                                <Button
+                                    size="sm"
+                                    variant="outline-primary"
+                                    type="button"
+                                    onClick={this.addHighSchool}
+                                    disabled={!String(this.state.currentHighSchool).match(/^([\w\-,.]+\s*)+$/g)}
+                                    className="btn-add-hs"
+                                >Add High School</Button>
+                            </Form.Row>
+                            <Form.Text>
+                                <div className="high-school-list">
+                                    Filtering: [<span> </span>
+                                    {
+                                        [...this.state.highSchools].map((highSchool) => {
+                                            return (
+                                                <div key={highSchool} style={{ display: "inline" }}>
+                                                    <span
+                                                        name={highSchool}
+                                                        className="high-school-unit"
+                                                        onMouseEnter={this.stylizeHoveredText}
+                                                        onClick={this.removeHighSchool}
+                                                        onMouseLeave={this.resetStyle}
+                                                    >{highSchool}</span>
+                                                    <span> </span>
+                                                </div>
+                                            );
+                                        })
+                                    }
+                                    <span> </span>]
+                                </div>
+                            </Form.Text>
+                            <Form.Text className="text-muted">To remove a high school from your filter, simply click on the name.</Form.Text>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Application Statuses</Form.Label>
+                            <Col>
+                            {
+                                ['Pending', 'Accepted', 'Denied', 'Deferred', 'Wait-listed', 'Withdrawn'].map((status) => {
+                                    return (
+                                        <Form.Check
+                                            inline
+                                            key={status}
+                                            type="checkbox"
+                                            label={status}
+                                            name={status.toLowerCase()}
+                                            onChange={this.handleAppStatuses}
+                                        />
+                                    )
+                                })
+                                }
+                            </Col>
+                        </Form.Group>
+                        <Button
+                            size="sm"
+                            variant="primary"
+                            type="submit"
+                            onClick={this.filter}
+                            className="btn-filter"
+                        >Apply Filters</Button>
+                        <Button
+                            size="sm"
+                            variant="outline-dark"
+                            name="scatterplot"
+                            type="button"
+                            onClick={this.toggleScatterplot}
+                            checked={this.state.scatterplot}
+                            className="ml-2"
+                        >{this.state.scatterplot ? 'View List' : 'View Scatterplot'}
+                        </Button>
+                    </Form>
                 </div>
                 {
                     this.state.scatterplot ?
-                        <Scatterplot students={this.state.students} college={this.props.college}/> :
-                    <div className="student-list">
-                        {this.state.students.map((student) => <AppTrackerItem key={student._id} student={student} />)}
-                    </div>
+                        <div className="mt-3">
+                            <Scatterplot students={this.state.students} college={this.props.college} />
+                        </div> :
+                        <div className="student-list mt-3">
+                            {this.state.students.map((student) => <AppTrackerItem key={student._id} student={student} />)}
+                        </div>
                 }
             </div>
         );
