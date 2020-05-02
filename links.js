c2402596f4554bc18373720fe9386035701cd486
hngan <hngan@cs.stonybrook.edu>
2020-05-02 00:05:49 -0400

    Added links

---
+++ links.js
@@ -0,0 +1,12 @@
+module.exports = {
+  links: {
+    nichelink: "http://allv22.all.cs.stonybrook.edu/~stoller/cse416/niche/",
+    THERankingLink:
+      "https://www.timeshighereducation.com/rankings/united-states/2020#!/page/0/length/-1/sort_by/rank/sort_order/asc/cols/stats",
+    THERankingDescriptionLink:
+      "https://www.timeshighereducation.com/world-university-rankings/",
+    //"http://allv22.all.cs.stonybrook.edu/~stoller/cse416/WSJ_THE/index.html",
+    collegeDataLink: "https://www.collegedata.com/college/",
+    //"http://allv22.all.cs.stonybrook.edu/~stoller/cse416/collegedata/",
+  },
+};
